c05d4f31d8e7942d0fa03f1e372227e4105e6fab
crypt096 <acvjetan@gmail.com>
2021-01-27 20:41:01 +0100

    Add tasks to server.

--- src/App.js
+++ src/App.js
@@ -24,14 +24,19 @@ function App() {
   }
 
   // Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:8000/tasks', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
 
-    const newTask = {
-      id, ...task
-    }
+      },
+      body: JSON.stringify(task)
+    })
 
-    setTasks([...tasks, newTask])
+    const data = await res.json();
+
+    setTasks([...tasks, data]);
   }
 
   // Delete Task
