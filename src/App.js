e3ac07e5266a5782bda0c600190c07751676e47d
crypt096 <acvjetan@gmail.com>
2021-01-27 20:10:42 +0100

    Show add task state.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([{
     "id": 1,
     "text": "Doctors Appointment",
@@ -23,6 +24,17 @@ function App() {
     "reminder": true
   }]);
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    
+    const newTask = {
+      id, ...task
+    }
+
+    setTasks([...tasks,newTask])
+  }
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -35,8 +47,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header/>
-      <AddTask/>
+      <Header onAdd={() => setShowAddTask(!showAddTask)}/>
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : 'No tasks to show'}
     </div>
   );
