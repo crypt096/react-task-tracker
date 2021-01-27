32df71551538ac82ce5c6fbe957e50e928e5e440
crypt096 <acvjetan@gmail.com>
2021-01-27 20:26:15 +0100

    Add json-server.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,32 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([{
-    "id": 1,
-    "text": "Doctors Appointment",
-    "day": "Feb 5th at 2:30pm",
-    "reminder": true
-  },
-  {
-    "id": 2,
-    "text": "Meeting at School",
-    "day": "Feb 6th at 1:30pm",
-    "reminder": false
-  },
-  {
-    "id": 3,
-    "text": "Meeting at Maldives",
-    "day": "Feb 16th at 00:00am",
-    "reminder": true
-  }]);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const res = await fetch('http://localhost:8000/tasks');
+      const data = await res.json();
+
+      console.log(data)
+    }
+
+    fetchTasks();
+  }, [])
 
   // Add Task
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) + 1;
-    
+
     const newTask = {
       id, ...task
     }
 
-    setTasks([...tasks,newTask])
+    setTasks([...tasks, newTask])
   }
 
   // Delete Task
@@ -42,12 +36,12 @@ function App() {
 
   // Toggle reminder
   const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder} : task))
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
   }
 
   return (
     <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : 'No tasks to show'}
     </div>
